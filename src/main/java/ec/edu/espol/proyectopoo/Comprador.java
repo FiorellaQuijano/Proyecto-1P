f7cae4845c7a4e165bd1246798b65c62ff6ea201
JosuMIguel <134465751+JosuMIguel@users.noreply.github.com>
2023-07-12 00:07:34 -0500

    Update Comprador.java

--- src/main/java/ec/edu/espol/proyectopoo/Comprador.java
+++ src/main/java/ec/edu/espol/proyectopoo/Comprador.java
@@ -20,11 +20,110 @@ public class Comprador extends Cliente{
         super(nombre, apellido, organizacion, correo, clave);
         ArrayList<Vehiculo> vehiculos = new ArrayList<>();
     }
-    
-    public ArrayList<Vehiculo> busquedaVehiculo(String tipo, double recorrido, int anio, double precio){
+
+ public ArrayList<Vehiculo> busquedaVehiculo(ArrayList<Vehiculo> vehiculos,String tipo, int anioInicio,int anioFin,  double recorridoInicio,double recorridoFin,double precioInicio, double precioFin){
         
         ArrayList<Vehiculo> vehiculosFiltro = new ArrayList<>();
         
-           
+        for(Vehiculo v: vehiculos){
+            int a= v.getAnio();
+            double r = v.getRecorrido();
+            double p = v.getPrecio();
+            
+            if(tipo.equals("moto")){
+                if(v instanceof Moto){
+                    if((a>anioInicio && a<anioFin)&&(r>recorridoInicio && r<recorridoFin )&&(p>precioInicio && p<precioFin))
+                        vehiculosFiltro.add(v);                    
+                }
+            }
+            else if (tipo.equals("auto")){
+                if(v instanceof Auto){
+                    if((a>anioInicio && a<anioFin)&&(r>recorridoInicio && r<recorridoFin )&&(p>precioInicio && p<precioFin))
+                        vehiculosFiltro.add(v);
+                }
+            }
+            else
+                System.out.println("no existe ese tipo de vehiculos en la compañia");
+        }
+        return vehiculosFiltro;    
     }
+    
+    public ArrayList<Vehiculo> busquedaVehiculo(ArrayList<Vehiculo> vehiculos,String tipo, double recorridoInicio,double recorridoFin, double precioInicio, double precioFin){
+        
+        ArrayList<Vehiculo> vehiculosFiltro = new ArrayList<>();
+        
+        for(Vehiculo v: vehiculos){
+            double r = v.getRecorrido();
+            double p = v.getPrecio();
+            
+            if(tipo.equals("moto")){
+                if(v instanceof Moto){
+                    if((r>recorridoInicio && r<recorridoFin )&&(p>precioInicio && p<precioFin))
+                        vehiculosFiltro.add(v);                    
+                }
+            }
+            else if (tipo.equals("auto")){
+                if(v instanceof Auto){
+                    if((r>recorridoInicio && r<recorridoFin )&&(p>precioInicio && p<precioFin))
+                        vehiculosFiltro.add(v);
+                }
+            }
+            else
+                System.out.println("no existe ese tipo de vehiculos en la compañia");
+        }
+        return vehiculosFiltro;    
+    }
+    
+    public ArrayList<Vehiculo> busquedaVehiculo(ArrayList<Vehiculo> vehiculos,String tipo, double precioInicio, double precioFin){
+        
+        ArrayList<Vehiculo> vehiculosFiltro = new ArrayList<>();
+        
+        for(Vehiculo v: vehiculos){
+            double p = v.getPrecio();
+            
+            if(tipo.equals("moto")){
+                if(v instanceof Moto){
+                    if(p>precioInicio && p<precioFin)
+                        vehiculosFiltro.add(v);                    
+                }
+            }
+            else if (tipo.equals("auto")){
+                if(v instanceof Auto){
+                    if(p>precioInicio && p<precioFin)
+                        vehiculosFiltro.add(v);
+                }
+            }
+            else
+                System.out.println("no existe ese tipo de vehiculos en la compañia");
+        }
+        return vehiculosFiltro;    
+    }
+    
+    public ArrayList<Vehiculo> busquedaVehiculo(ArrayList<Vehiculo> vehiculos,String tipo){
+        
+        ArrayList<Vehiculo> vehiculosFiltro = new ArrayList<>();
+        
+        for(Vehiculo v: vehiculos){
+            
+            if(tipo.equals("moto")){
+                if(v instanceof Moto){
+                    vehiculosFiltro.add(v);                    
+                }
+            }
+            else if (tipo.equals("auto")){
+                if(v instanceof Auto){
+                    vehiculosFiltro.add(v);
+                }
+            }
+            else
+                System.out.println("no existe ese tipo de vehiculos en la compañia");
+        }
+        return vehiculosFiltro;
+        
+        
+    }
+    
+
+ 
+   
 }
