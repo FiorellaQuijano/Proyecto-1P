816184747ef3792356fdf95c85cbfac41b83262c
JosuMIguel <134465751+JosuMIguel@users.noreply.github.com>
2023-07-12 00:08:53 -0500

    Update Vehiculo.java

--- src/main/java/ec/edu/espol/proyectopoo/Vehiculo.java
+++ src/main/java/ec/edu/espol/proyectopoo/Vehiculo.java
@@ -18,4 +18,89 @@ public class Vehiculo {
     private String color;
     private String tipoCombustible;
     private double precio;
+
+    public Vehiculo(String placa, String marca, String modelo, String tipoMotor, int anio, double recorrido, String color, String tipoCombustible, double precio) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipoMotor = tipoMotor;
+        this.anio = anio;
+        this.recorrido = recorrido;
+        this.color = color;
+        this.tipoCombustible = tipoCombustible;
+        this.precio = precio;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getTipoMotor() {
+        return tipoMotor;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public double getRecorrido() {
+        return recorrido;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setTipoMotor(String tipoMotor) {
+        this.tipoMotor = tipoMotor;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public void setRecorrido(double recorrido) {
+        this.recorrido = recorrido;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
 }
