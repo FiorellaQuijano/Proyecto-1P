f104df6f3de540b39dceac80297f475e175498e3
Domaalva <139288690+Domaalva@users.noreply.github.com>
2023-07-14 09:24:03 -0500

    Update Cliente.java

--- src/main/java/ec/edu/espol/proyectopoo/Cliente.java
+++ src/main/java/ec/edu/espol/proyectopoo/Cliente.java
@@ -14,6 +14,8 @@ public class Cliente {
     private String organizacion;
     private String correo;
     private String clave;
+    private static ArrayList<Cliente> clientes = new ArrayList<>();
+
 
     public Cliente(String nombre, String apellido, String organizacion, String correo, String clave) {
         this.nombre = nombre;
@@ -35,23 +37,46 @@ public class Cliente {
         String correo1;
         
         do{
-            System.out.println("ingrese correo: ");
+            System.out.println("Ingrese correo: ");
             correo1 = sc.nextLine();
             
+            boolean correoExistente=false;
             for(Cliente c:clientes){
                 if(correo1.equals(c.correo)){
                     System.out.println("Ya existe el correo");
-                    cr=c.correo;
+                    correoExistente=true;
+                    break;
                 }
-                
-            }           
-        }while(correo1.equals(cr));
+            
+            }
+            if(!correoExistente){
+                break;
+            }
+        }while(true);
          
-        System.out.println("ingrese clave: ");
+        System.out.println("Ingrese clave: ");
         String clave = sc.nextLine();
-        
         return new Cliente(nombre,apellido,organizacion,correo1,clave);
-        
     }
+    public static byte[] getSHA(String input)throws NoSuchAlgorithmException{
+        MessageDigest md=MessageDigest.getInstance("SHA-256");
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
     
+    }
+    public static String toHexString(byte[] hash){
+        BigInteger number=new BigInteger(1,hash);
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+        while (hexString.length() < 64)
+        {
+            hexString.insert(0, '0');
+        }
+ 
+        return hexString.toString();
+    }
+
+            
+        
+    
+
+   
 }
