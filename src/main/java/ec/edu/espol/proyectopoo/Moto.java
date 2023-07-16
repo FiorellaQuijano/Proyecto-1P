aed54f450ee5b23d1c157a9a743312e62a6eb414
FiorellaQuijano <139190414+FiorellaQuijano@users.noreply.github.com>
2023-07-15 22:51:02 -0500

    Update Moto.java
    nuevo método agregar moto

--- src/main/java/ec/edu/espol/proyectopoo/Moto.java
+++ src/main/java/ec/edu/espol/proyectopoo/Moto.java
@@ -9,11 +9,39 @@ package ecu.edu.espol.proyectopoo;
  * @author aquin
  */
 public class Moto extends Vehiculo{
-
     public Moto(String placa, String marca, String modelo, String tipoMotor, int anio, double recorrido, String color, String tipoCombustible, double precio) {
         super(placa, marca, modelo, tipoMotor, anio, recorrido, color, tipoCombustible, precio);
     }
     
+    public String toString(){
+        return "MOTO,"+super.toString();
+    }
+    
+    public static void agregarVehiculo(Moto m){
+        FileWriter fw= null;
+        BufferedWriter bfw= null;
+        try{
+           fw= new FileWriter("Archivo.csv",true);
+           bfw= new BufferedWriter(fw);
+           bfw.write(m.toString()+"\n");
+           System.out.println("\nSe ha registrado correctamente");
+        }catch(IOException e){
+            System.out.println("\nError abriendo el archivo: " + e);
+        }finally {
+            try{
+                if (bfw != null) {
+                    bfw.close();
+            }
+                if (fw != null) {
+                    fw.close();
+                }
+                
+            }catch(IOException e2){
+                System.out.println("\nNo se pudo cerrar el archivo: " +e2);
+            }
+        }
+        
+    }
     
     
 }
